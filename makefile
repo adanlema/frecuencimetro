fc506144db70a39dc2d51a287a1f075c310857ce
adanlema <adanlema@hotmail.com>
2023-07-21 20:48:00 -0300

    Primer commit realizados 	nuevos archivos: doxyfile 	nuevos archivos: makefile 	nuevos archivos: src/main.c

---
+++ makefile
@@ -0,0 +1,17 @@
+MODULES = module/freertos
+BOARD ?= bluepill
+MUJU ?= ./muju
+
+include $(MUJU)/module/base/makefile
+
+OUT_DIR = ./build
+DOC_DIR = $(OUT_DIR)/doc
+
+$(OUT_DIR):
+	mkdir $(OUT_DIR)
+
+$(DOC_DIR) : $(OUT_DIR)
+	mkdir $(DOC_DIR)
+
+doc: $(DOC_DIR)
+	doxygen doxyfile
---
+++ src/main.c
@@ -0,0 +1,22 @@
+/* Copyright 2023, Adan Lema <adanlema@hotmail.com> */
+
+/*==================[inclusions]=============================================*/
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+
+/** @ doxygen end group definition */
+/** @ doxygen end group definition */
+/** @ doxygen end group definition */
+/*==================[end of file]============================================*/
